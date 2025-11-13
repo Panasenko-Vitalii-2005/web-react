104147e56d2a17004c6f9bb358df56a641ff1990
PanasenkoVitalii <panasenko14102005@gmail.com>
2025-11-13 20:41:17 +0200

    commit

---
+++ src/components/Sidenav.jsx
@@ -0,0 +1,14 @@
+export default function Sidenav(){
+    return (
+        <section className="sidenav">
+            <aside className="sidebar">
+                <h3 className="sidebar__title">Меню</h3>
+                <ul className="sidevar__list">
+                    <li><a href="#">Новини</a><a/></li>
+                    <li><a href="#">Довідка</a><a/></li>
+                    <li><a href="#">Налаштування</a><a/></li>
+                </ul>
+            </aside>
+        </section>
+    )
+}
